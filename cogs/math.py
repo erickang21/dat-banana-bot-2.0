5989e95d97a68e9a505725654fe44511fed2b460
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2017-12-08 16:11:58 -0500

    Create math.py

---
+++ cogs/math.py
@@ -0,0 +1,49 @@
+import discord
+import sys
+import os
+import io
+from discord.ext import commands
+
+
+class math:
+    def __init__(self, bot):
+    self.bot = bot
+    
+    
+    @commands.command()
+    async def add(self, ctx, num: int, num2: int):
+            '''It...ADDS? Yea. Usage: *add [no.1] [no.2]'''
+            if num is None:
+                await ctx.send("Aren't you stupid enough? Usage: *add [no.1] [no.2]")
+            else:
+                await ctx.send(num + num2)
+                
+    @commands.command()
+    async def subtract(self, ctx, num: int, num2: int):
+            '''That big MINUS. Usage: *subtract [no.1] [no.2]'''
+            if num is None:
+                await ctx.send("Aren't you stupid enough? Usage: *subtract [no.1] [no.2]")
+            else:
+                await ctx.send(num - num2)
+                
+                
+    @commands.command()
+    async def multiply(self, ctx, num: int, num2: int):
+            '''Multiply em. Git ready! Usage: *multiply [no.1] [no.2]'''
+            if num is None:
+                await ctx.send("Aren't you stupid enough? Usage: *multiply [no.1] [no.2]")
+            else:
+                await ctx.send(num * num2)
+                
+                
+    @commands.command()
+    async def divide(self, ctx, num: int, num2: int):
+            '''Divide em. Cut em. Idc. Usage: *multiply [no.1] [no.2]'''
+            if num is None:
+                await ctx.send("Aren't you stupid enough? Usage: *divide [no.1] [no.2]")
+            else:
+                await ctx.send(num / num2)
+                
+                
+def setup(bot): 
+    bot.add_cog(math(bot))  
