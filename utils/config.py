c48ddd3de511d464e3ebf7ac2f9875a0bb7025a5
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2017-12-08 16:25:44 -0500

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,6 @@
+bot_icon = str("https://cdn.discordapp.com/avatars/387706175770198023/6a4d8feab850ae38615818b0c0046774.jpg")
+owner_icon = str("https://cdn.discordapp.com/avatars/277981712989028353/da3e726b6908af64b89b1140e85713ce.jpg")
+owner_id = 277981712989028353
+bot_id = 387706175770198023
+passcolor = int(0x00FF00)
+failcolor = int(0xFF0000)
