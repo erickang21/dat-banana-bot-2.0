2e23959a614fd9f651793269d7a397ce7132e7f9
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2017-12-12 17:03:16 -0500

    added alias for eightball: 8ball

--- bot.py
+++ bot.py
@@ -102,7 +102,7 @@ async def timer(ctx, timer):
         await ctx.send("TIME'S UP! :clock:")
 
         
-@bot.command()
+@bot.command(aliases=['8ball'])
 async def eightball(ctx, *, message:str):
     """Really desperate? Ask the 8ball for advice. Only yes/no questions!"""
     choices = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes, definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', ' Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Do not count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
