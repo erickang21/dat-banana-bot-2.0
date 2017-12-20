8cd00f7fe140410f07bb57f7acea8954385307e2
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2017-12-19 21:01:28 -0500

    imported cog Fun

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ bot = commands.Bot(command_prefix='*',description="The revamped dat banana bot m
 bot.load_extension("cogs.Math")
 bot.load_extension("cogs.Mod")
 bot.load_extension("cogs.Utility")
+bot.load_extension("cogs.Fun")
 
 
 def cleanup_code(content):
@@ -75,119 +76,7 @@ async def _set(ctx, Type=None,*,thing=None):
       else:
         await ctx.send('Want me to do something? YOU do it right first. Usage: *presence [game/stream] [msg]')
 
-        
-@bot.command()
-async def hack(ctx, user: discord.Member):
-    """Hack someone's account! Try it!"""
-    msg = await ctx.send(f"Hacking! Target: {user}")
-    await asyncio.sleep(2)
-    await msg.edit(content="Accessing Discord Files... [▓▓    ]")
-    await asyncio.sleep(2)
-    await msg.edit(content="Accessing Discord Files... [▓▓▓   ]")
-    await asyncio.sleep(2)
-    await msg.edit(content="Accessing Discord Files... [▓▓▓▓▓ ]")
-    await asyncio.sleep(2)
-    await msg.edit(content="Accessing Discord Files COMPLETE! [▓▓▓▓▓▓]")
-    await asyncio.sleep(2)
-    await msg.edit(content="Retrieving Login Info... [▓▓▓    ]")
-    await asyncio.sleep(3)
-    await msg.edit(content="Retrieving Login Info... [▓▓▓▓▓ ]")
-    await asyncio.sleep(3)
-    await msg.edit(content="Retrieving Login Info... [▓▓▓▓▓▓ ]")
-    await asyncio.sleep(4)
-    await msg.edit(content=f"An error has occurred hacking {user}'s account. Please try again later. ❌")
-    
-      
-@bot.group(aliases=['animation', 'a'])
-async def anim(ctx, Type):
-    """Animations! Usage: *anim [type]. For a list, use *anim list."""
-    if Type is None:
-        await ctx.send('Probably a really cool animation, but we have not added them yet! But hang in there! You never know... For a current list, type *anim list')
-    else:
-        if Type.lower() == 'wtf':
-          msg = await ctx.send("```W```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```WO```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```WOT```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```WOT D```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```WOT DA```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```WOT DA F```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```WOT DA FU```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```WOT DA FUK```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```WOT DA FUK!```")
-          await asyncio.sleep(1)
-          await msg.edit(content="WOT DA FUK!")
-        elif Type.lower() == 'mom':
-          msg = await ctx.send("```Y```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```YO```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```YO M```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```YO MO```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```YO MOM```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```YO MOM!```")
-          await asyncio.sleep(1)
-          await msg.edit(content="YO MOM!")
-        elif Type.lower() == 'gethelp':
-          msg = await ctx.send("```STOP!```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```STOP! G```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```STOP! Ge```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```STOP! Get```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```STOP! Get s```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```STOP! Get so```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```STOP! Get som```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```STOP! Get some```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```STOP! Get some HELP```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```STOP! Get some HELP!!!```")
-          await asyncio.sleep(1)
-          await msg.edit(content="STOP! Get some HELP!!!")
-        elif Type.lower() == 'sike':
-          msg = await ctx.send("```W```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```Wa```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```Wai```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```Wait```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```Wait.```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```Wait..```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```Wait...```")
-          await asyncio.sleep(1)
-          await msg.edit(content="```SIKE!```")
-          await asyncio.sleep(1)
-          await msg.edit(content="SIKE!")
-        elif Type.lower() == 'list':
-          color = discord.Color(value=0x00ff00)
-          em=discord.Embed(color=color, title="Current List of Awesome Animations:")
-          em.description = "wtf (*anim wtf), mom (*anim mom), gethelp (*anim gethelp), sike (*anim sike)."
-          em.set_footer(text="We will always be adding new animations!")
-          await ctx.send(embed=em)
-        else:
-          await ctx.send('Probably a really cool animation, but we have not added them yet! But hang in there! You never know... For a current list, type *anim list')              
-
-                         
+                
 @bot.command()
 async def bug(ctx, *, msg:str):
     """Does this bot have a bug? Report it and we squash it."""
@@ -199,20 +88,7 @@ async def bug(ctx, *, msg:str):
     await lol.send(embed=em)                     
     await ctx.send("Thanks for reporting that bug! :bug: We will get back to you ASAP.")                     
  
-                         
-@bot.command(aliases=['8ball'])
-async def eightball(ctx, *, message:str):
-    """Really desperate? Ask the 8ball for advice. Only yes/no questions!"""
-    choices = ['It is certain. :white_check_mark:', 'It is decidedly so. :white_check_mark:', 'Without a doubt. :white_check_mark:', 'Yes, definitely. :white_check_mark:', 'You may rely on it. :white_check_mark:', 'As I see it, yes. :white_check_mark:', 'Most likely. :white_check_mark:', ' Outlook good. :white_check_mark:', 'Yes. :white_check_mark:', 'Signs point to yes. :white_check_mark:', 'Reply hazy, try again. :large_orange_diamond: ', 'Ask again later. :large_orange_diamond: ', 'Better not tell you now. :large_orange_diamond: ', 'Cannot predict now. :large_orange_diamond: ', 'Concentrate and ask again. :large_orange_diamond: ', 'Do not count on it. :x:', 'My reply is no. :x:', 'My sources say no. :x:', 'Outlook not so good. :x:', 'Very doubtful. :x:']
-    color = discord.Color(value=0xeaff29)
-    em=discord.Embed(color=color, title=f"{message}", description=random.choice(choices))
-    em.set_author(name="The Mighty 8 ball", icon_url="https://vignette.wikia.nocookie.net/battlefordreamislandfanfiction/images/5/53/8-ball_my_friend.png/revision/latest?cb=20161109021012")
-    em.set_footer(text=f"Sent by {ctx.message.author.name}")
-    await ctx.message.delete()
-    await ctx.send(embed=em)
-    
-    
-    
+                
 @bot.command()
 async def say(ctx, *, message:str):
     '''I say what you want me to say. Oh boi...'''
@@ -220,7 +96,6 @@ async def say(ctx, *, message:str):
     await ctx.send(message)                   
                        
 
-        
 @bot.command()
 async def invite(ctx):
     """Allow my bot to join the hood. YOUR hood."""
@@ -231,7 +106,6 @@ async def invite(ctx):
 async def _discord(ctx):
     """We have an awesome hood to join, join now!"""
     await ctx.send("Your turn to join the hood -> https://discord.gg/wvkVknA")
-
 
     
 @bot.command()
