d04aeb183885df4d1befad7a65d174329e44f68a
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2017-12-20 16:54:55 -0500

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -196,12 +196,12 @@ class fun:
 
     
     @commands.command()
-    async def annoy(self, ctx, user: discord.Member, times: int):
+    async def annoy(self, ctx, member: discord.Member, times: int):
         """Annoy! GR... Usage: *annoy [user] [no. of times]."""
         await ctx.send("Please be advised. This command will limit itself to 1 msg/second to avoid heavy ratelimits, which stops sending messages if the msg/sec is too high. This bot may still get ratelimited, i.e. slow down or stop for a while, please be patient. The annoy will start 3 secs after this msg is sent.")
         await asyncio.sleep(3)
         for x in range(times):
-            await ctx.send(user)
+            await ctx.send(member.mention)
             asyncio.sleep(1)
        
     
