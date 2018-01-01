67fcb22a3d1ac45a0ce5dbb6cea19ac578b9fa2f
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-01-01 17:55:54 -0500

    Create clashroyale.py

---
+++ cogs/clashroyale.py
@@ -0,0 +1,15 @@
+import discord
+import sys
+import os
+import io
+import clashroyale
+from discord.ext import commands
+
+
+class clashroyale:
+    def __init__(self, bot):
+        self.bot = bot
+        
+        
+    @commands.command()
+    async def crprofile
