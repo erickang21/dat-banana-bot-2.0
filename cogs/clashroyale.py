172218392c8c22d2f0e08e6d3cb479f59c49e384
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-01-01 18:38:18 -0500

    Update clashroyale.py

--- cogs/clashroyale.py
+++ cogs/clashroyale.py
@@ -12,4 +12,17 @@ class clashroyale:
         
         
     @commands.command()
-    async def crprofile
+    async def crprofile(self, ctx, crtag:str):
+        """Shows CR stats for you! Usage: *crprofile [tag]"""
+        client = clashroyale.Client("607d4e53a8b643f1bbb7837bacb7ec3c4706bc9420b34377a869d8048500f998", is_async=True)
+        profile = await client.get_player(crtag)
+        color = discord.Color(value=0xf1f442)
+        em = discord.Embed(color=color, title=profile.name)
+        em.description = f"#{profile.tag}"
+        em.add_field(name='Trophies', value=profile.trophies)
+        em.add_field(name='Personal Best', value=profile.stats.max_trophies)
+        em.add_field(name='XP Level', value=profile.stats.level)
+        em.add_field(name='Arena', value=profile.arena.name)
+        em.add_field(name='Wins/Losses/Draws', value=record)
+        em.add_field(name='Win Rate', value=f"{(profile.games.wins / (profile.games.wins + profile.games.losses) * 100):.3f}%")
+        
