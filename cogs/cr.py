0cda620351154317447e6fe9469cf8d95cfceb91
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-01-04 15:15:58 -0500

    Update cr.py

--- cogs/cr.py
+++ cogs/cr.py
@@ -15,17 +15,26 @@ class cr:
     @commands.command()
     async def crprofile(self, ctx, crtag:str):
         """Shows CR stats for you! Usage: *crprofile [tag]"""
-        profile = await self.client.get_player(crtag)
-        color = discord.Color(value=0xf1f442)
-        em = discord.Embed(color=color, title=f'{profile.name}')
-        em.description = f'#{profile.tag}' 
-        em.add_field(name='Trophies', value=f'{profile.trophies}')
-        em.add_field(name='Personal Best', value=f'{profile.stats.max_trophies}')
-        em.add_field(name='XP Level', value=f'{profile.stats.level}')
-        em.add_field(name='Arena', value=f'{profile.arena.name}')
-        em.add_field(name='Wins/Losses/Draws', value=f'{profile.games.wins}/{profile.games.draws}/{profile.games.losses}')
-        em.add_field(name='Win Rate', value=f'{(profile.games.wins / (profile.games.wins + profile.games.losses) * 100):.3f}%')
-        await ctx.send(embed=em)
+        if crtag is None:
+            await ctx.send('Please enter your tag. Usage: *crprofile [tag].') # Bot hosts on Heroku, which means there is no save tag feature (Heroku deletes data every day.)
+        else:
+            profile = await self.client.get_player(crtag)
+            clan = await profile.get_clan()
+            color = discord.Color(value=0xf1f442)
+            em = discord.Embed(color=color, title=f'{profile.name}')
+            em.description = f'#{profile.tag}'
+            if profile.clan.role:
+                em.set_author(name=f'{clan.name}')
+            else:
+                em.set_author(name='No Clan')
+            em.add_field(name='Trophies', value=f'{profile.trophies}')
+            em.add_field(name='Personal Best', value=f'{profile.stats.max_trophies}')
+            em.add_field(name='XP Level', value=f'{profile.stats.level}')
+            em.add_field(name='Arena', value=f'{profile.arena.name}')
+            em.add_field(name='Wins/Losses/Draws', value=f'{profile.games.wins}/{profile.games.draws}/{profile.games.losses}')
+            em.add_field(name='Win Rate', value=f'{(profile.games.wins / (profile.games.wins + profile.games.losses) * 100):.3f}%')
+            em.set_thumbnail(url=f'https://cr-api.github.io/cr-api-assets/arenas/arena{profile.arena.arenaID}.png')
+            await ctx.send(embed=em)
         
 
 def setup(bot): 
