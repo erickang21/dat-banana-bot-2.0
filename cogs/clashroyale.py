1f924696e42df61a4078d675e5b86db1ce201c91
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-01-03 21:49:01 -0500

    just tryin to get clashroyale to work

--- cogs/clashroyale.py
+++ cogs/clashroyale.py
@@ -17,14 +17,14 @@ class clashroyale:
         client = clashroyale.Client("607d4e53a8b643f1bbb7837bacb7ec3c4706bc9420b34377a869d8048500f998", is_async=True)
         profile = await client.get_player(crtag)
         color = discord.Color(value=0xf1f442)
-        em = discord.Embed(color=color, title=profile.name)
-        em.description = f"#{profile.tag}"
-        em.add_field(name='Trophies', value=profile.trophies)
-        em.add_field(name='Personal Best', value=profile.stats.max_trophies)
-        em.add_field(name='XP Level', value=profile.stats.level)
-        em.add_field(name='Arena', value=profile.arena.name)
-        em.add_field(name='Wins/Losses/Draws', value=record)
-        em.add_field(name='Win Rate', value=f"{(profile.games.wins / (profile.games.wins + profile.games.losses) * 100):.3f}%")
+        em = discord.Embed(color=color, title=f'{profile.name}')
+        em.description = f'#{profile.tag}' 
+        em.add_field(name='Trophies', value=f'{profile.trophies}')
+        em.add_field(name='Personal Best', value=f'{profile.stats.max_trophies}')
+        em.add_field(name='XP Level', value=f'{profile.stats.level}')
+        em.add_field(name='Arena', value=f'{profile.arena.name}')
+        em.add_field(name='Wins/Losses/Draws', value=f'{record}')
+        em.add_field(name='Win Rate', value=f'{(profile.games.wins / (profile.games.wins + profile.games.losses) * 100):.3f}%')
         await ctx.send(embed=em)
         
 
