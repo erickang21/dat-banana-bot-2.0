3aef2a843ce270efdc169cb501cbc197aa319d19
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-01-05 15:13:47 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def on_ready():
         await asyncio.sleep(15)
         await bot.change_presence(game=discord.Game(name="using *help!"))
         await asyncio.sleep(15)
-        await bot.change_presence(game=discord.Game(name="in v4.0, BETA."))
+        await bot.change_presence(game=discord.Game(name="in v4.0.1, BETA."))
         await asyncio.sleep(15)
 
 @bot.event
