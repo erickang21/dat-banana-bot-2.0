837f23d49f09f863d85384462277a868d80844b8
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-01-05 15:00:21 -0500

    Completely overhauled cr stats.

--- cogs/cr.py
+++ cogs/cr.py
@@ -27,8 +27,9 @@ class cr:
             em.add_field(name='Arena', value=f'{profile.arena.name}')
             em.add_field(name='Wins/Losses/Draws', value=f'{profile.games.wins}/{profile.games.draws}/{profile.games.losses}')
             em.add_field(name='Win Rate', value=f'{(profile.games.wins / (profile.games.wins + profile.games.losses) * 100):.3f}%')
+            em.add_field(name='Favorite Card', value=f'{profile.stats.favorite_card.name})                                                                                                                                                 
             em.set_author(name='Stats')
-            em.set_thumbnail(url=f'https://cr-api.github.io/cr-api-assets/arenas/arena{profile.arena.arenaID}.png')
+            em.set_thumbnail(url=f'https://cr-api.github.io/cr-api-assets/arenas/arena{profile.arena.arenaID}.png') # This allows thumbnail to match your arena! Maybe it IS possible after all...
             em.set_footer(text='API: cr-api.com', icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
             await ctx.send(embed=em)
             profile = await self.client.get_player(crtag)
@@ -39,8 +40,8 @@ class cr:
             color = discord.Color(value=0xf1f442)
             if profile.clan.role:
                 em = discord.Embed(color=color)
-                em.description = f'{clan.name} ({clan.tag})'
-                em.add_field(name='Role', value=f'{profile.clan.role})
+                em.description = f'{clan.name} (#{clan.tag})'
+                em.add_field(name='Role', value=f'{profile.clan.role})                                                                                                                                                                      
                 em.add_field(name='Clan Score', value=f'{clan.score}')
                 em.add_field(name='Members', value=f'{len(clan.members)}/50')
                 em.set_thumbnail(url=clan.badge.image)
@@ -48,10 +49,21 @@ class cr:
                 await ctx.send(embed=em)
             else:
                 em.description = 'No Clan'
-                em.set_thumbnail(url='http://i1253.photobucket.com/albums/hh599/bananaboy21/maxresdefault_zpseuid4jln.jpg')
+                em.set_thumbnail(url='http://i1253.photobucket.com/albums/hh599/bananaboy21/maxresdefault_zpseuid4jln.jpg') # This is the url for the No Clan symbol.
                 em.set_footer(text='API: cr-api.com', icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
                 await ctx.send(embed=em)
-        
+            profile = await self.client.get_player(crtag)
+            color = discord.Color(value=0xf1f442)
+            em = discord.Embed(color=color)
+            em.add_field(name='Challenge Max Wins'), value=f'{profile.stats.challenge_max_wins}')
+            em.add_field(name='Challenge Cards Won', value=f'{profile.stats.challenge_cards_won}')
+            em.add_field(name='Tourney Cards Won', value=f'{profile.stats.tournament_cards_won}')
+            em.set_author(name='Challenge/Tourney Stats')
+            em.set_thumbnail(url=http://vignette4.wikia.nocookie.net/clashroyale/images/a/a7/TournamentIcon.png/revision/latest?cb=20160704151823)
+            await ctx.send(embed=em)
+
+
+
                          
             
         
