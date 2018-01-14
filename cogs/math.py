d06c57975961c2abdfd9ee8bf6783bdf77442ba3
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-01-14 17:42:36 -0500

    trying to fix calc command

--- cogs/math.py
+++ cogs/math.py
@@ -51,38 +51,45 @@ class math:
     @commands.command()
     async def calc(self, ctx, num1: int, sign, num2: int):
         '''Does some simple math for you.'''
+        if num1 is None:
+            await ctx.send("You are missing a number. Missing Arg: num1")
+        if num2 is None:
+            await ctx.send("You are missing a number. Missing Arg: num2")
+        if sign is None:
+            await ctx.send("Please enter a sign. +, -, x, /. Missing Arg: sign \nExample: *calc 3 + 4")
         try:
             float(num1)
             float(num2)
         except ValueError:
-            await ctx.send("Enter your formula like this: *calc [number] [+|-|x|/] [number] \n Example: *calc 3 + 4")
+            await ctx.send("Enter your formula like this: *calc [number] [+|-|x|/] [number] \nExample: *calc 3 + 4")
         else:
-            if sign is None:
-                await ctx.send("Please enter a sign. +, -, x, /. \n Example: *calc 3 + 4")
+            if sign == '+':
+                color = discord.Color(value=0x00ff00)
+                em = discord.Embed(color=color, title='Calculator')
+                em.add_field(name='Input:', value=f'{num1}+{num2}')
+                em.add_field(name='Output:', value=f'{num1 + num2}')
+                await ctx.send(embed=em)
+            if sign == '-':
+                color = discord.Color(value=0x00ff00)
+                em = discord.Embed(color=color, title='Calculator')
+                em.add_field(name='Input:', value=f'{num1}-{num2}')
+                em.add_field(name='Output:', value=f'{num1 - num2}')
+                await ctx.send(embed=em)
+            if sign == 'x':
+                color = discord.Color(value=0x00ff00)
+                em = discord.Embed(color=color, title='Calculator')
+                em.add_field(name='Input:', value=f'{num1}x{num2}')
+                em.add_field(name='Output:', value=f'{num1 * num2}')
+                await ctx.send(embed=em)
+            if sign == '/':
+                color = discord.Color(value=0x00ff00)
+                em = discord.Embed(color=color, title='Calculator')
+                em.add_field(name='Input:', value=f'{num1}+{num2}')
+                em.add_field(name='Output:', value=f'{num1 + num2}')
+                await ctx.send(embed=em)
             else:
-                if sign == '+':
-                    color = discord.Color(value=0x00ff00)
-                    em = discord.Embed(color=color, title='Calculator')
-                    em.add_field(name='Input:', value=f'{num1}+{num2}')
-                    em.add_field(name='Output:', value=f'{num1 + num2}')
-                if sign == '-':
-                    color = discord.Color(value=0x00ff00)
-                    em = discord.Embed(color=color, title='Calculator')
-                    em.add_field(name='Input:', value=f'{num1}-{num2}')
-                    em.add_field(name='Output:', value=f'{num1 - num2}')
-                if sign == 'x':
-                    color = discord.Color(value=0x00ff00)
-                    em = discord.Embed(color=color, title='Calculator')
-                    em.add_field(name='Input:', value=f'{num1}x{num2}')
-                    em.add_field(name='Output:', value=f'{num1 * num2}')
-                if sign == '/':
-                    color = discord.Color(value=0x00ff00)
-                    em = discord.Embed(color=color, title='Calculator')
-                    em.add_field(name='Input:', value=f'{num1}+{num2}')
-                    em.add_field(name='Output:', value=f'{num1 + num2}')
-                else:
-                    await ctx.send("Please enter a valid sign: +, -, x, / \n Example: *calc 3 + 4")
-                    
+                await ctx.send("Please enter a valid sign: +, -, x, / \n Example: *calc 3 + 4")
+
 
 def setup(bot): 
     bot.add_cog(math(bot))  
