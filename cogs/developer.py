644dc0e570b3c4917603423a966feb717bce5c6e
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-01-13 20:52:43 -0500

    fixed the dev_check stuff in developer.py

--- cogs/developer.py
+++ cogs/developer.py
@@ -2,6 +2,7 @@ import discord
 import sys
 import os
 import io
+import pip
 import asyncio
 import aiohttp
 import random
@@ -14,19 +15,19 @@ class developer:
        self.bot = bot
 
 
-    def dev_check(id):
-        with open('data/devs.json') as f:
-            devs = json.load(f)
-            if id in devs:
-                return True
-            return False
+    def dev_check(self, id):
+    with open('data/devs.json') as f:
+        devs = json.load(f)
+    if id in devs:
+        return True
+    return False
        
        
        
     @commands.command()
     async def restart(self, ctx):
         """Makes the bot shut UP and then shut DOWN, then start up again."""
-        if not dev_check(ctx.author.id):
+        if not self.dev_check(ctx.author.id):
             return await ctx.send("HALT! This command is for the devs only. Sorry. :x:")
         msg = await ctx.send("Shutting down...")
         await asyncio.sleep(1)
@@ -37,7 +38,7 @@ class developer:
     @commands.command()
     async def changename(self, ctx, name:str):
         """Changes my name. Please make it good!"""
-        if not dev_check(ctx.author.id):
+        if not self.dev_check(ctx.author.id):
             return await ctx.send("HALT! This command is for the devs only. Sorry. :x:")
         if name is None:
             return await ctx.send("Hmm...my name cannot be blank!")
@@ -48,7 +49,7 @@ class developer:
     @commands.command()
     async def exec(self, ctx, code):
         """Executes code like the Command Line."""
-        if not dev_check(ctx.author.id):
+        if not self.dev_check(ctx.author.id):
             return await ctx.send("HALT! This command is for the devs only. Sorry. :x:")
         await ctx.send(subprocess.run(f"{code}",stdout=subprocess.PIPE).stdout.decode('utf-8'))
             
