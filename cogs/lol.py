e9b2b21d4d89d3ea19ac78fb382ab727c0990dd4
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-01-20 08:29:09 -0500

    fix

--- cogs/lol.py
+++ cogs/lol.py
@@ -20,19 +20,22 @@ class lol:
         self.bot = bot
 
 
-
     @commands.command()
     async def lolprofile(self, ctx, lolname:str):
-    	"""Gets League of Legends stats! Usage: *lolprofile [summoner name]"""
-    	cass.set_riot_api_key('RGAPI-338c02bc-134d-4055-b846-41045a553b9b')
+        """Gets League of Legends stats! *lolprofile [name]"""
+        cass.set_riot_api_key('RGAPI-cda7f225-9ca9-4cb5-a932-9c9c7f7f7455')
         cass.set_default_region("NA")
-		color = discord.Color(value=0xf1f442)  	
-    	summoner = cass.get_summoner(name=lolname)
-    	check = summoner.exists
-    	if check == 'False':
-    		return await ctx.send("That is an invalid LoL summoner name. Check it, please! :x:")
-    	em = discord.Embed(color=color, title=f'{summoner.name}')
-    	em.add_field(name='Summoner Level', value=f'{summoner.level}')
-    	em.add_field(name='Summoner ID', value=f'{summoner.id}')
-    	em.set_thumbnail(url=f'{summoner.profile_icon.url}')
-    	await ctx.send(embed=em)
+        color = discord.Color(value=0xf1f442)
+        summoner = cass.get_summoner(name=lolname)
+        check = summoner.exists
+        if check is False:
+            return await ctx.send("Invalid LoL summoner name. :x:")
+        em = discord.Embed(color=color, title=f'{summoner.name}')
+        em.add_field(name='Summoner Level', value=f'{summoner.level}')
+        em.add_field(name='Summoner ID', value=f'{summoner.id}')
+        em.set_thumbnail(url=f'{summoner.profile_icon.url}')
+        await ctx.send(embed=em)
+
+
+def setup(bot): 
+    bot.add_cog(lol(bot)) 
