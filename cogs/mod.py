aa31fe3a67fcc7144ed98402e80c6558cebc9b8e
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-02-12 16:52:17 -0500

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -12,14 +12,19 @@ class mod:
         
         
     @commands.command()
+    @commands.has_permissions(administrator = True)
     async def dm(self, ctx, user: discord.Member, *, msg: str):
         """Escort your DM to someone thru the bot. Usage: *dm [tag person] [msg]"""
         try:
             await user.send(msg)
             await ctx.message.delete()            
             await ctx.send("SuccESS! Your DM has made it! :white_check_mark: ")
+        except discord.ext.commands.MissingPermissions:
+            await ctx.send("Aw, come on! You thought you could get away with DM'ing people without permissions.")
         except:
             await ctx.send("Error :x:. Make sure your message is shaped in this way: *dm [tag person] [msg]")
+
+
     
     
     @commands.command()
@@ -32,6 +37,8 @@ class mod:
             await user.send(embed=em)
             await ctx.message.delete()
             await ctx.send("User has been DM'd :white_check_mark:. Pray that the user is a gud boi now. :pray:")
+        except discord.ext.commands.MissingPermissions:
+            await ctx.send("Aw, come on! You thought you could get away with warning people without permissions.")
         except:
             await ctx.send("Something happened and the DM could not make it :x:. The user could be blocking DMs from the server, or you did not use the format correctly. Usage: *warn [tag person] [reason].")    
        
@@ -48,17 +55,26 @@ class mod:
                 except ValueError:
                     return await ctx.send("The number is invalid. Make sure it is valid! Usage: *purge [number of msgs]")
                 await ctx.channel.purge(limit=num+1)
-                await ctx.send("Purged successfully :white_check_mark:")
+                msg = await ctx.send("Purged successfully :white_check_mark:")
+                await asyncio.sleep(3)
+                await msg.delete()
         except discord.Forbidden:
             await ctx.send("Purge unsuccessful. The bot does not have Manage Msgs permission.")
+        except discord.ext.commands.MissingPermissions:
+            await ctx.send("Aw, come on! You thought you could get away with purging without permissions.")
     
     
     @commands.command()
     @commands.has_permissions(kick_members = True)
     async def kick(self, ctx, user: discord.Member):
         """Kicks a member into the world outside your server."""
-        await ctx.channel.send(f"Be gone {user.name}! Oh, and close the door on the way out :door:.")
-        await user.kick()
+        try:
+            await ctx.channel.send(f"Be gone {user.name}! Oh, and close the door on the way out :door:.")
+            await user.kick()
+        except discord.Forbidden:
+            await ctx.send("Oops! I don't have enough permissions to kick this bad boi.")
+        except discord.ext.commands.MissingPermissions:
+            await ctx.send("Aw, come on! You thought you could get away with kicking someone without permissions.")
         
         
     
@@ -66,43 +82,47 @@ class mod:
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, user: discord.Member):
         """Swings the mighty Ban Hammer on that bad boy."""
-        await ctx.channel.send(f"The ban hammer has fallen. And it has struck {user.name}.")
-        await user.ban()
+        try:
+            await ctx.channel.send(f"The ban hammer has fallen. And it has struck {user.name}.")
+            await user.ban()
+        except discord.Forbidden:
+            await ctx.send("Oops! I don't have enough permissions to swing this ban hammer.")
+        except discord.ext.commands.MissingPermissions:
+            await ctx.send("Aw, come on! You thought you could get away with banning someone without permissions.")
 
 
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def mute(self, ctx, user: discord.Member, time: int):
+    async def mute(self, ctx, user: discord.Member, mutetime=None):
         '''Forces someone to shut up. Usage: *mute [user] [time in mins]'''
         try:
-            if time is None:
+            if mutetime is None:
                 await ctx.channel.set_permissions(user, send_messages=False)
                 await ctx.send(f"{user.mention} is now forced to shut up. :zipper_mouth: ")
             else:
                 try:
-                    time = time * 60
-                    float(time)
+                    mutetime =int(mutetime)
+                    mutetime = mutetime * 60
                 except ValueError:
                     return await ctx.send("Your time is an invalid number. Make sure...it is a number.")
-                await ctx.guild.set_permissions(user, send_messages=False)
+                await ctx.channel.set_permissions(user, send_messages=False)
                 await ctx.channel.send(f"{user.mention} is now forced to shut up. :zipper_mouth: ")
-                await asyncio.sleep(time)
-                await ctx.guild.set_permissions(user, send_messages=True)
+                await asyncio.sleep(mutetime)
+                await ctx.channel.set_permissions(user, send_messages=True)
                 await ctx.channel.send(f"{user.mention} is now un-shutted up.")
         except discord.Forbidden:
             return await ctx.send("I could not mute the user. Make sure I have the manage channels permission.")
+        except discord.ext.commands.MissingPermissions:
+            await ctx.send("Aw, come on! You thought you could get away with shutting someone up without permissions.")
 
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def unmute(self, ctx, user: discord.Member):
         '''Allows someone to un-shut up. Usage: *unmute [user]'''
-        if user is None:
-            await ctx.send("Dang. You must enter a user to un-shut them up!")
-        else:
-            await ctx.guild.set_permissions(user, send_messages=True)
-            await ctx.channel.send(f"{user.mention} is now un-shutted up.")
+        await ctx.channel.set_permissions(user, send_messages=True)
+        await ctx.channel.send(f"{user.mention} is now un-shutted up.")
         
         
     @commands.command()
